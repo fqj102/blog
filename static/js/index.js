0c538c37b27b7e257415b5a3d4b6cc511aa7d870
qingji fang <fqj102@gmail.com>
2020-05-07 02:22:13 +0900

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -93,7 +93,7 @@ function categoryDisplay() {
     /*show category when click categories list*/
     $('.categories-list-item').click(function() {
         var cate = $(this).attr('cate'); //get category's name
-
+        alert(cate);
         $('.post-list-body>div[post-cate!=' + cate + ']').hide(250);
         $('.post-list-body>div[post-cate=' + cate + ']').show(400);
     });
